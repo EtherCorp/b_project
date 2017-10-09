0708994d565e204c604827ab66b3fd0230af63ae
cristiannsc <cristian.sepulveda.c@usach.cl>
2017-10-09 19:33:58 -0300

    Added scraping from Zmart (PS4 y XBONE)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/zmartPS4', to: 'scraping#zmart_ps4_scrape'
+  get '/zmartXBONE', to: 'scraping#zmart_xbone_scrape'
+  get '/weplay', to: 'scraping#weplay_scrape'
 end
