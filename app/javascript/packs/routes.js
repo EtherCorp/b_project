07674211dc6126d3395975334283793c456bcc39
René Zárate Meneses <rene.zarate@usach.cl>
2017-12-11 18:55:30 -0300

    Change BrowserRoouter to HashRouter

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { HashRouter,BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import App from "./components/App";
 
 import GameViewContainer from './containers/game-view-container'
 
 export default () => {
  return (
-   <BrowserRouter>
-   <Switch>
-   <Route exact path="/" component={App}/>
-   <Route path="/games/:id" component={GameViewContainer}/>
-   {/*<Route path="*" to="/" />*/}
-   </Switch>
-   </BrowserRouter>
+   <HashRouter>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route path="/games/:id" component={GameViewContainer}/>
+      {/*<Route path="*" to="/" />*/}
+    </Switch>
+   </HashRouter>
  )
 }
